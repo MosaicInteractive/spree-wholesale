e865e9027fd1690729c1dba06db0aa7bc2706c97
Peter Kordel <pkordel@gmail.com>
2010-07-10 16:12:50 +0200

    fixed table check

--- lib/wholesale/product.rb
+++ lib/wholesale/product.rb
@@ -1,8 +1,8 @@
 module Wholesale::Product
   def self.included(model)
     model.class_eval do
-      if ActiveRecord::Base.connection.table_exists?(Variant.to_s)
-        delegate_belongs_to :master, :wholesale_price if Variant.column_names('wholesale_price')
+      if ActiveRecord::Base.connection.table_exists?('variants')
+        delegate_belongs_to :master, :wholesale_price if Variant.column_names.include?('wholesale_price')
       end
     end
   end
