733fef210bc7cf7bc00e8aeac1b528ff22dce587
Peter Kordel <pkordel@gmail.com>
2010-07-10 15:59:57 +0200

    fix for bootstraping

--- lib/wholesale/product.rb
+++ lib/wholesale/product.rb
@@ -1,7 +1,9 @@
 module Wholesale::Product
   def self.included(model)
     model.class_eval do
-      delegate_belongs_to :master, :wholesale_price
+      if ActiveRecord::Base.connection.table_exists?(Variant.to_s)
+        delegate_belongs_to :master, :wholesale_price if Variant.column_names('wholesale_price')
+      end
     end
   end
 end
--- wholesale_extension.rb
+++ wholesale_extension.rb
@@ -4,7 +4,7 @@
 class WholesaleExtension < Spree::Extension
   version "0.1"
   description "Add wholesale pricing to products"
-  url "http://github.com/mlambie/spree-wholesale"
+  url "http://github.com/pkordel/spree-wholesale"
 
   # Please use wholesale/config/routes.rb instead for extension routes.
 
@@ -13,22 +13,6 @@ class WholesaleExtension < Spree::Extension
   # end
   
   def activate
-
-    # Add your extension tab to the admin.
-    # Requires that you have defined an admin controller:
-    # app/controllers/admin/yourextension_controller
-    # and that you mapped your admin in config/routes
-
-    #Admin::BaseController.class_eval do
-    #  before_filter :add_yourextension_tab
-    #
-    #  def add_yourextension_tab
-    #    # add_extension_admin_tab takes an array containing the same arguments expected
-    #    # by the tab helper method:
-    #    #   [ :extension_name, { :label => "Your Extension", :route => "/some/non/standard/route" } ]
-    #    add_extension_admin_tab [ :yourextension ]
-    #  end
-    #end
     
     # Reopen the Product class and delegate wholesale_price (first variant)
     Product.send(:include, Wholesale::Product)
