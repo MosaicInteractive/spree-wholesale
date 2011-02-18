8fd4cd66b258e06e0052d9ac08c4247b113d8f62
Mosaic Interactive <valentino@mosaicwebsite.com>
2011-02-18 08:46:34 -0500

    Added zone_pricing compatability

--- lib/wholesale/products_helper.rb
+++ lib/wholesale/products_helper.rb
@@ -7,7 +7,20 @@ module Wholesale::ProductsHelper
         if (!current_user.nil? && current_user.has_role?("wholesale") && !product_or_variant.wholesale_price.blank?)          
           amount = product_or_variant.wholesale_price
         else
-          amount = product_or_variant.price
+          ##############################################
+          # ZONE_PRICING compatability
+          ##############################################
+          # Check if this object is a product, if so then access the master variant
+          # record for the product to get the zone pricing
+          object = product_or_variant.is_a?(Product) ? product_or_variant.master : product_or_variant
+
+          # Get the zone price for this variant/product if one is defined
+          # otherwise use the normal price
+          if object.respond_to?(:zone_price)
+            amount = object.zone_price(get_user_country_id)
+          else
+            amount = object.price
+          end
         end
         amount += Calculator::Vat.calculate_tax_on(product_or_variant) if Spree::Config[:show_price_inc_vat]
         options.delete(:format_as_currency) ? format_price(amount, options) : ("%0.2f" % amount).to_f
